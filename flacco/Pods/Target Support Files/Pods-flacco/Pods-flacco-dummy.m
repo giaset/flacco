322b654234199bb635cbb39e3bb7e6f0fbc24eda
Gianni Settino <g@miltonandparc.com>
2016-06-20 07:32:08 -0500

    Cocoapods and RealmSwift

---
+++ flacco/Pods/Target Support Files/Pods-flacco/Pods-flacco-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_flacco : NSObject
+@end
+@implementation PodsDummy_Pods_flacco
+@end
---
+++ flacco/Pods/Target Support Files/Pods-flacco/Pods-flacco-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_flaccoVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_flaccoVersionString[];
+
