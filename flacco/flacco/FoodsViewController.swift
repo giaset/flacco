d4897fc7f1459a22e29bc2292f12d908a8e39702
Gianni Settino <g@miltonandparc.com>
2016-06-20 07:06:34 -0500

    Create FoodsViewController

---
+++ flacco/flacco/FoodsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoodsViewController.swift
+//  flacco
+//
+//  Created by Gianni Settino on 2016-06-20.
+//  Copyright © 2016 Milton and Parc. All rights reserved.
+//
+
+import UIKit
+
+class FoodsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.redColor()
+    }
+}
